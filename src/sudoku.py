90a651e46f9f3260c58a2d392e016143a61ae09b
frsouzaa <felipe.souza1506@outlook.com>
2022-10-17 12:17:22 -0300

    inicio do desenvolvimento

--- src/sudoku.py
+++ src/sudoku.py
@@ -11,17 +11,74 @@ class Sudoku():
 
 
     def __init__(self):
-        self.tabuleiro = [[], [], [], [], [], [], [], [], []]
-        self.colunas = [[], [], [], [], [], [], [], [], []]
-        self.blocos = [[], [], [], [], [], [], [], [], []]
+        self.tabuleiro = [[], [], [], 
+                          [], [], [], 
+                          [], [], []]
+        self.colunas = [[], [], [], 
+                        [], [], [], 
+                        [], [], []]
+        self.blocos = [[], [], [], 
+                       [], [], [], 
+                       [], [], []]
 
     
     def preencherTabuleiro(self):
-        opcoes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        for i in range(9):
-            for j in range(9):
-                self.tabuleiro[i].append(choice(opcoes))
+        try:
+            for i in range(9):
+                for j in range(9):
+                    teste = j
+                    opcoes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+                    if i < 3 and j < 3:
+                        bloco = 0
+                    if i < 3 and 3 <= j < 6:
+                        bloco = 1
+                    if i < 3 and j >= 6:
+                        bloco = 2
+                    
+                    if 3 <= i < 6 and j < 3:
+                        bloco = 3
+                    if 3 <= i < 6 and 3 <= j < 6:
+                        bloco = 4
+                    if 3 <= i < 6 and j >= 6:
+                        bloco = 5
+                    
+                    if i >= 6 and j < 3:
+                        bloco = 6
+                    if i >= 6 and 3 <= j < 6:
+                        bloco = 7
+                    if i >= 6 and j >= 6:
+                        bloco = 8
+
+                    if bloco == 1 or bloco == 4 or bloco == 7:
+                        novasOpcoes = list(set(self.colunas[-1] + self.colunas[-2] + self.colunas[-3]))
+                        if novasOpcoes: opcoes = novasOpcoes
+                        
+
+                    for numero in self.tabuleiro[i]:
+                        if numero in opcoes: opcoes.remove(numero)
+
+                    for numero in self.colunas[j]:
+                        if numero in opcoes: opcoes.remove(numero)
+                    
+                    for numero in self.blocos[bloco]:
+                        if numero in opcoes: opcoes.remove(numero)
+                    
+                    numeroSorteado = choice(opcoes)
+                    # print(F"{j} {numeroSorteado} > {opcoes}")
+                    # print(bloco)
+
+                    self.tabuleiro[i].append(numeroSorteado)
+                    self.colunas[j].append(numeroSorteado)
+                    self.blocos[bloco].append(numeroSorteado)
+
+                print(f"{i} > {self.tabuleiro[i]}")
+                    # print(self.tabuleiro[i])
+                    # for i in range(9):
+                    #     print(self.tabuleiro[i])
+                    # print("")
+        except:
+            print(f"j > {teste} {opcoes}")
 
     def printTabuleiro(self):
         for i in range(9):
